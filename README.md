20b535b1622a2cfbf10a31ccc0044bbab37a8327
Mozaks <64087432+Mozaks@users.noreply.github.com>
2023-12-01 15:01:03 +0300

    Commit
    Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aston.practice
+
+For a description of all APIs, data and its use see - [swagger](http://localhost:8080/swagger-ui/index.html#/)
--- src/main/java/com/example/practice/AstonPracticeApplication.java
+++ src/main/java/com/example/practice/AstonPracticeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AstonPracticeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AstonPracticeApplication.class, args);
 	}
